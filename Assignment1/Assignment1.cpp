7303a8f4d53d22875a76f467dfe23ebfb6fa49ac
UmbrellaCorp99 <ayoung6@mail.umw.edu>
2025-05-23 11:20:25 -0400

    Add project files.

---
+++ Assignment1/Assignment1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <stdio.h>
+#include "logic.h"
+#include <allegro5/allegro.h>
+
+int main()
+{
+	std::cout << "Hello World" << std::endl;
+}
---
+++ Assignment1/logic.h
@@ -0,0 +1,12 @@
+class logic {
+public:
+	logic();
+	void setWord(string newWord);
+	string getWord();
+
+private:
+	string word;
+	string scramble;
+	string words[100];
+
+};
