31541173856a730ebeea3850e213293728fdb634
UmbrellaCorp99 <ayoung6@mail.umw.edu>
2025-05-23 11:31:58 -0400

    Files set up and logic class scaffolding from Canvas used

--- Assignment1/logic.h
+++ Assignment1/logic.h
@@ -1,12 +1,26 @@
 class logic {
 public:
-	logic();
-	void setWord(string newWord);
-	string getWord();
-
+    logic();
+    //constructor
+    void introduction();
+    //Provides a basic introduction to the user
+    bool createLists();
+    //Opens a file and reads in the list of words that can be scrambled
+    //Returns: true if words inserted and false if not
+    bool playGame();
+    //Carries out the actual word scrambling
+    //Returns: true if user wins and false if not
+    string scrambler(string word);
+    //Takes in a word and scrambles it
+    //Returns a scrambled version of the parameter
+    void end();
+    //Displays the endgame information
 private:
-	string word;
-	string scramble;
-	string words[100];
-
-};
+    int numCorrect,             //the number of correct words
+        smallWordLength,       //length of word arrays
+        mediumWordLength,
+        largeWordLength;
+    string smallWords[50];   //array of words of length 4-5
+    string mediumWords[50];   //array of words of length 6-7
+    string largeWords[50];   //array of words of length 8+   
+};//end class logic
