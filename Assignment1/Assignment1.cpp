5ff9362839a8b4f57ea1903f99767a5f12076194
UmbrellaCorp99 <ayoung6@mail.umw.edu>
2025-05-24 12:20:23 -0400

    Added allegro timer and input threads

--- Assignment1/Assignment1.cpp
+++ Assignment1/Assignment1.cpp
@@ -2,11 +2,38 @@
 #include "logic.h"
 #include <allegro5/allegro.h>
 
+using namespace std;
+
+void* input(ALLEGRO_THREAD* ptr, void* arg);
+void* timer(ALLEGRO_THREAD* ptr, void* arg);
+bool finished = false;
+bool timeOut = false;
+logic game;
+
 int main()
 {
-	logic game;
 	game.introduction();
 	game.createLists();
 	game.end();
 	return 0;
 }
+
+void* input(ALLEGRO_THREAD* ptr, void* arg) {
+	finished = false;
+	cout << "Asking for user input?";
+	cin >> finished;
+	finished = true;
+	return NULL;
+}
+
+void* timer(ALLEGRO_THREAD* ptr, void* arg) {
+	time_t startTime, currentTime;
+	startTime = time(NULL);
+	currentTime = time(NULL);
+	while (currentTime - startTime < 60 && !finished) {
+		currentTime = time(NULL);
+	}
+	timeOut = true;
+	return NULL;
+}
+
